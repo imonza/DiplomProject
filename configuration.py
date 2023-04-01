c5ff27918eafa602137ef468f0c51f81ce123c40
Artem Krasnoshtanov <art.mkd@yandex.ru>
2023-04-01 18:36:31 +0400

    Diplom_project

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://0bc6ae99-3719-47e3-b5fb-17035c7d6842.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+GET_ORDER_BY_NUMBER = "/api/v1/orders/track?t={track_number}"
---
+++ data.py
@@ -0,0 +1,14 @@
+order_body = {
+        "firstName": "Olga",
+        "lastName": "Uchiha",
+        "address": "Moscow",
+        "metroStation": 4,
+        "phone": "+78003333333",
+        "rentTime": 5,
+        "deliveryDate": "2023-03-26",
+        "comment": "Saske, come back to Konoha",
+        "color": ["BLACK"]
+}
+
+headers = {'Content-Type': 'application/json'}
+
